af1ddaa93dece8b58a83650b794fa08f92b7b567
bakkerjoeri <bakkerjoeri@gmail.com>
2023-03-08 16:46:05 +0100

    ESM compatability

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { UnaryFunction } from './UnaryFunction.js';
+export type { UnaryFunction } from './UnaryFunction.js';
 export { compose } from './compose.js';
 export { pipe } from './pipe.js';
